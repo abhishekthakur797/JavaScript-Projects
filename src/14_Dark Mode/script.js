bc03416675b6cff34d2f3ae503a2934ca7f26c04
Abhishek <officialabhishekthakur@gmail.com>
2025-07-02 00:27:07 +0530

    some minor change

--- src/14_Dark Mode/script.js
+++ src/14_Dark Mode/script.js
@@ -1,20 +1,20 @@
 const themeToggle = document.getElementById("theme-toggle");
-const container = document.getElementById("container");
+const body = document.body;
 
 themeToggle.addEventListener("click", function () {
   if (
-    container.classList.contains("bg-white") &&
-    container.classList.contains("text-black")
+    body.classList.contains("bg-white") &&
+    body.classList.contains("text-black")
   ) {
-    container.classList.remove("bg-white", "text-black");
-    container.classList.add("bg-black", "text-white");
-    themeToggle.classList.remove("fa-sun");
-    themeToggle.classList.add("fa-moon");
-  } else {
-    container.classList.remove("bg-black", "text-white");
-    container.classList.add("bg-white", "text-black");
+    body.classList.remove("bg-white", "text-black");
+    body.classList.add("bg-black", "text-white");
     themeToggle.classList.remove("fa-moon");
     themeToggle.classList.add("fa-sun");
+  } else {
+    body.classList.remove("bg-black", "text-white");
+    body.classList.add("bg-white", "text-black");
+    themeToggle.classList.remove("fa-sun");
+    themeToggle.classList.add("fa-moon");
   }
 });
 
